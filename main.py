209888ecfb4253d9e92ddefd70ec4e73a9691426
graddon <132186309+graddon@users.noreply.github.com>
2023-06-03 20:13:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-
 import subprocess
+from colorama import init, Fore, Style
 
-
-
+init(autoreset=True)
 
 options = {
     "1": ["Starting Token Checker...", ["python", "modules/checker.py"]],
@@ -18,13 +17,10 @@ options = {
     "11": ["Starting Nitro Checker...", ["python", "modules/nitro_checker.py"]],
     "12": ["Starting Vouch",  ["python", "modules/vouch.py"]],
     "13": ["..................",  ["python", "modules/pinger.py"]],
-    "14": ["..................",  ["python", "modules/discordbotnuker.py"]],        
-    "15": ["=.=",  ["python", "modules/credits.py"]]    
+    "14": ["..................",  ["python", "modules/discordbotnuker.py"]],    
+    "15": ["=.=",  ["python", "modules/serverclonermenu.py"]],        
+    "16": ["=.=",  ["python", "modules/credits.py"]]    
 }
-
-from colorama import init, Fore, Style
-
-init(autoreset=True)
 
 banner = f"""
 {Fore.MAGENTA}
@@ -40,22 +36,17 @@ banner = f"""
 {' ' * 42}made by GR#1111
 {Style.RESET_ALL}
 
-
-{' ' * 14}{Fore.MAGENTA}╔═════════════════════════════════════════════════════════════════════════════╗{Style.RESET_ALL}
-{' ' * 14}{Fore.MAGENTA}║  {Fore.CYAN}[1] Token Checker        {Fore.MAGENTA}  {Fore.CYAN}[6] Webhook Spammer    {Fore.MAGENTA}  {Fore.CYAN}[11] Nitro Checker     {Fore.MAGENTA}║{Style.RESET_ALL}
-{' ' * 14}{Fore.MAGENTA}║  {Fore.CYAN}[2] Leave Servers        {Fore.MAGENTA}  {Fore.CYAN}[7] Webhook Deleter    {Fore.MAGENTA}  {Fore.CYAN}[12] Vouches           {Fore.MAGENTA}║{Style.RESET_ALL}
-{' ' * 14}{Fore.MAGENTA}║  {Fore.CYAN}[3] Onliner              {Fore.MAGENTA}  {Fore.CYAN}[8] Hypesquad          {Fore.MAGENTA}  {Fore.CYAN}[13] Ping Raid         {Fore.MAGENTA}║{Style.RESET_ALL}
-{' ' * 14}{Fore.MAGENTA}║  {Fore.CYAN}[4] Nuker                {Fore.MAGENTA}  {Fore.CYAN}[9] Raid               {Fore.MAGENTA}  {Fore.CYAN}[14] Bot Nuker         {Fore.MAGENTA}║{Style.RESET_ALL}
-{' ' * 14}{Fore.MAGENTA}║  {Fore.CYAN}[5] Token Info           {Fore.MAGENTA}  {Fore.CYAN}[10] Nitro Generator   {Fore.MAGENTA}  {Fore.CYAN}[15] Credits           {Fore.MAGENTA}║{Style.RESET_ALL}
-{' ' * 14}{Fore.MAGENTA}╚═════════════════════════════════════════════════════════════════════════════╝{Style.RESET_ALL}
+{' ' * 10 + Fore.MAGENTA + '╔══════════════════════════════════════════════════════════════════════════════════════════════════════╗' + Style.RESET_ALL}
+{' ' * 10 + Fore.MAGENTA + '║  ' + Fore.CYAN + '[1] Token Checker        ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[6] Webhook Spammer    ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[11] Nitro Checker     ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[16] Credits           ' + Fore.MAGENTA + '║' + Style.RESET_ALL}
+{' ' * 10 + Fore.MAGENTA + '║  ' + Fore.CYAN + '[2] Leave Servers        ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[7] Webhook Deleter    ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[12] Vouches           ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[17] Soon              ' + Fore.MAGENTA + '║' + Style.RESET_ALL}
+{' ' * 10 + Fore.MAGENTA + '║  ' + Fore.CYAN + '[3] Onliner              ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[8] Hypesquad          ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[13] Ping Raid         ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[18] Soon              ' + Fore.MAGENTA + '║' + Style.RESET_ALL}
+{' ' * 10 + Fore.MAGENTA + '║  ' + Fore.CYAN + '[4] Nuker                ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[9] Raid               ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[14] Bot Nuker         ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[15] Soon              ' + Fore.MAGENTA + '║' + Style.RESET_ALL}
+{' ' * 10 + Fore.MAGENTA + '║  ' + Fore.CYAN + '[5] Token Info           ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[10] Nitro Generator   ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[15] Server Cloner     ' + Fore.MAGENTA + '  ' + Fore.CYAN + '[19] Soon              ' + Fore.MAGENTA + '║' + Style.RESET_ALL}
+{' ' * 10 + Fore.MAGENTA + '╚══════════════════════════════════════════════════════════════════════════════════════════════════════╝' + Style.RESET_ALL}
 """
-
 print(banner)
 
-
-{Fore.MAGENTA}
 user_input = input("Select an option: ")
-{Style.RESET_ALL}
 
 if user_input in options:
     selected_option = options[user_input]
@@ -63,4 +54,3 @@ if user_input in options:
     subprocess.run(selected_option[1])
 else:
     print("Invalid option selected.")
- 
